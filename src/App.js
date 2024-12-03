caf12820fe763492be31ad44613c74cd122acd2f
lenmelon <lenmelon@gmail.com>
2024-12-03 18:28:58 +0000

    Errors fixed and HTML added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
+import React from "react";
+import Weather from "./Weather";
 import "./App.css";
 
 export default function App() {
   return (
     <div className="App">
       <div className="container">
-        <h1>Weather App</h1>
+        <Weather />
         <footer>
           This project was created by Elena F. an is open-sourced on{" "}
-          <a href="https://github.com/lenmelon" target="_blank">
+          <a
+            href="https://github.com/lenmelon"
+            target="_blank"
+            rel="noreferrer"
+          >
             Github
           </a>
         </footer>
