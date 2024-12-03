86b7cf2b2f777d029337ea6e0071fcb8dae2029b
lenmelon <lenmelon@gmail.com>
2024-12-03 17:50:53 +0000

    Bootstrap added, edits made to other pages

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from "./logo.svg";
 import "./App.css";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project was created by Elena F. an is open-sourced on{" "}
+          <a href="https://github.com/lenmelon" target="_blank">
+            Github
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
-
-export default App;
